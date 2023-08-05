eaa8efa1f055f2694072b438fec89f6e4dfde394
RedAngelRedux <sammy@sammynava.com>
2023-08-05 15:36:59 -0700

    Create initial version of the Tag Model

---
+++ Models/Tag.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoderHive.Models
+{
+    public class Tag
+    {
+        // Key Properties
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        // Class Properties
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be at least {2} and nomore than {1}", MinimumLength = 2)]
+        public string Text { get; set; }
+
+        // Navigation Properies
+        public virtual Post Post { get; set; }
+
+        public virtual IdentityUser Author { get; set; }
+
+
+    }
+}
