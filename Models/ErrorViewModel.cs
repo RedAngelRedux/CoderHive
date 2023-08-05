9591bfce99f7c3f6d5bb1c8a3f708d51563738fc
RedAngelRedux <sammy@sammynava.com>
2023-08-05 13:54:22 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoderHive.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
