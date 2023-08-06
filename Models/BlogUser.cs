50d41601531c1b5060ffdf85f8cbff017023a4e4
RedAngelRedux <sammy@sammynava.com>
2023-08-05 18:01:54 -0700

    Extend IdentityUser with BlogUser

---
+++ Models/BlogUser.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CoderHive.Models
+{
+    public class BlogUser : IdentityUser
+    {
+
+        // Class Properties
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at most {1} characters", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = "";
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at most {1} characters", MinimumLength = 2)]
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; } = "";
+
+        [Display(Name = "User Image")]
+        public byte[] ImageData { get; set; }
+
+        [Display(Name = "Image Type")]
+        public string ImageType { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters", MinimumLength = 6)]
+        public string FacebookUrl { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters", MinimumLength = 6)]
+        public string TwitterUrl { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters", MinimumLength = 6)]
+        public string LinkedInUrl { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters", MinimumLength = 6)]
+        public string GitHubUrl { get; set; }
+
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        // Navigation Properties
+
+        // Parent of...
+        public virtual ICollection<Blog> Blogs { get; set; } = new HashSet<Blog>();
+
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+    }
+}
