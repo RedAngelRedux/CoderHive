35a6beb7438ae4adfe5c59d896ee1e2a49413b18
RedAngelRedux <sammy@sammynava.com>
2023-08-20 17:11:01 -0400

    Code Generated, Examined, and Changed Through the "Index Action/View" lesson of the "Blog Controller/Views Series".  Blogs/Index Route verified to work up to this point.

--- Program.cs
+++ Program.cs
@@ -10,18 +10,20 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 //builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //    options.UseSqlServer(connectionString));
+
+// // Replace Default Reference to SQL Server with our Postgres Database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
-
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddIdentity<BlogUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddDefaultUI()
+    .AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
