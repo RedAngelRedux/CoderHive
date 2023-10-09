159e46571bf1a511c4671c51f2770f526b7478c5
RedAngelRedux <sammy@sammynava.com>
2023-10-08 23:18:43 -0700

    Added a js function to make sure no duplicate tags are allowed and to handle other errors related to tag management from within a post.  Also replaced the default alert windows with SweetAlert2

--- wwwroot/js/posts.js
+++ wwwroot/js/posts.js
@@ -5,9 +5,26 @@ function AddTag() {
     // Get a reference to the TagEntry input element
     let tagEntry = document.getElementById("TagEntry");
 
-    // Create a new Select Option
-    let newOption = new Option(tagEntry.value, tagEntry.value);
-    document.getElementById("TagList").options[index++] = newOption;
+    // Use the Search function to detect an error state
+    let searchResult = Search(tagEntry.value);
+    if (searchResult != null) {
+
+        // Trigger Sweet Alert
+    //    Swal.fire({
+    //        title: 'Error!',
+    //        text: `${searchResult}`,
+    //        icon: 'error',
+    //        confirmButtonText: 'Cool'
+    //    })
+        swalWithDarkButton.fire({
+            html: `<span class='font-weight-bolder'>${searchResult.toUpperCase()}</span>`
+        })
+    }
+    else {
+        // Create a new Select Option
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+        document.getElementById("TagList").options[index++] = newOption;
+    }
 
     // Clear out the TagEntry control
     tagEntry.value = "";
@@ -15,18 +32,42 @@ function AddTag() {
 }
 
 function DeleteTag() {
+
     let tagCount = 1;
+    let tagList = document.getElementById("TagList");
+    if (!tagList) return false;
+
+    if (tagList.selectedIndex == -1) {
+        swalWithDarkButton.fire({
+            html: "<span class='font-weight-bolder'>CHOOSE A TAG BEFORE DELETING</span>"
+        });
+        return true;
+    }
 
     while (tagCount > 0) {
-        let selectedIndex = document.getElementById("TagList").selectedIndex;
-        if (selectedIndex >= 0) {
-            document.getElementById("TagList").options[selectedIndex] = null;
+        if (tagList.selectedIndex >= 0) {
+            tagList.options[tagList.selectedIndex] = null;
             --tagCount;
         }
         else {
             tagCount = 0;
         }
         index--;
+    }
+}
+
+// The Search function will detect either an empty or a duplicate Tag
+// and return an error string if an error is detected
+function Search(str) {
+
+    if (str == "") return 'Empty tags are not permitted';
+
+    let tagList = document.getElementById("TagList");
+    if (tagList) {
+        let options = tagList.options;
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].value == str) return `The tag #${str} is a duplicate`;
+        }
     }
 }
 
@@ -49,3 +90,12 @@ function ReplaceTag(tag, index) {
     let newOption = new Option(tag, tag);
     document.getElementById("TagList").options[index] = newOption;
 }
+
+const swalWithDarkButton = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-danger btn-sm btn-outline-dark'
+    },
+    imageUrl: '/images/RedX.png',
+    timer: 3000,
+    buttonsStyling: false
+})
